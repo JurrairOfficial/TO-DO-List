6f8c42aecbf8c7a13986fdd8d543da567dc71043
Jurrair <m.jurrairfreelancer66@gamil.com>
2025-06-05 20:38:49 +0500

    design are added

---
+++ ja.js
@@ -0,0 +1,13 @@
+// const inputtext = document.querySelector("#inputtext");
+// const btn       = document.querySelector("#btn");
+// const todoList  = document.querySelector("#todoList");
+
+
+// btn.addEventListener('click', ()=>{
+    
+//     const inputText = ariaValueMax.trim();
+//     if(inputtext.Length <= 0 )[
+//         alert("you must enter some thing")
+//     ]
+
+// })
