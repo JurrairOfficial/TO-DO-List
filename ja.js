e092f96c86a8f65696db722cccecbe1caed2dd5d
Jurrair <m.jurrairfreelancer66@gamil.com>
2025-06-11 10:00:45 +0500

    almost all logic has done

--- ja.js
+++ ja.js
@@ -1,13 +1,42 @@
-// const inputtext = document.querySelector("#inputtext");
-// const btn       = document.querySelector("#btn");
-// const todoList  = document.querySelector("#todoList");
+const inputtext = document.querySelector("#inputtext");
+const btn       = document.querySelector("#btn");
+const todoList  = document.querySelector("#todoList");
 
 
-// btn.addEventListener('click', ()=>{
+
+
+
+const addtodo =  ()=>{
     
-//     const inputText = ariaValueMax.trim();
-//     if(inputtext.Length <= 0 )[
-//         alert("you must enter some thing")
-//     ]
+    const inputbox = inputtext.value.trim();
+    if(inputbox.length <= 0 ){
+        alert("you must enter some thing");
+        return false;
+    
+    }
+    const li = document.createElement("li");
+    const p  = document.createElement("p");
+    p.innerHTML = inputbox;
+    li.appendChild(p);
+    todoList.appendChild(li);
+    inputtext.value = "";
+    
+    const deletbutton     = document.createElement("deletbutton");
+    deletbutton.innerText = "Remove";
+    deletbutton.classList.add("deletbutton");
+    li.appendChild(deletbutton);
 
-// })
+    const editbutton     = document.createElement("editbutton");
+    editbutton.innerText = "Edit";
+    editbutton.classList.add("editbutton");
+    li.appendChild(editbutton);
+
+
+
+
+
+
+
+}
+
+btn.addEventListener('click', addtodo);
